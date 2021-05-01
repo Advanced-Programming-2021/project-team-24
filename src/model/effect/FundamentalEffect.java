bce9700eaf056856549b732c334023be174114a3
alirezahaqi <ali123haghi@gmail.com>
2021-05-01 21:27:47 +0430

    Add files via upload

--- src/model/effect/FundamentalEffect.java
+++ src/model/effect/FundamentalEffect.java
@@ -6,5 +6,6 @@ public abstract class FundamentalEffect {
     private List<String> requiredPhase;
     private List<String> requiredEvent;
     private String requiredString;    
-    public abstract boolean isConditionsSatisfied();    
+    public abstract boolean isConditionsSatisfied();
+    public abstract void runEffect();           
 }
