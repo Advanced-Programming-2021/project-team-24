5dfe870f4061206bdee6bddaea00432ee0acdc17
alirezahaqi <ali123haghi@gmail.com>
2021-05-01 15:22:39 +0430

    added global for scanner in view folder

---
+++ src/view/Global.java
@@ -0,0 +1,10 @@
+package view;
+
+import java.util.*;
+
+public class Global {
+    public static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        
+    }
+}
