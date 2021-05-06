703477e96f2c8bb17dc6b42d9d6b7044006a2126
behzad <behzadnabawi@gmail.com>
2021-05-06 12:20:43 +0430

    Effect.java

--- src/model/effect/Effect.java
+++ src/model/effect/Effect.java
@@ -1,5 +1,11 @@
 package model.effect;
 
+import controller.Message;
+
 public class Effect {
-    
+    private EffectType typeOfEffect;
+    private String requirement;
+    private boolean askForActivation;
+    private String name;
+    private String askAbleMessage;
 }
