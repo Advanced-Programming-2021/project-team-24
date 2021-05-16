5fd2c1c95e9ce8522d703c46f512f47109c0d063
alirezahaqi <ali123haghi@gmail.com>
2021-05-16 23:18:04 +0430

    git bug

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,39 +5,23 @@ import model.card.Card;
 import model.user.User;
 import view.LoginMenu;
 
-
+import org.junit.Assert;
+import org.junit.Test;
 
 
 public class App {
     public static void main(String[] args) throws Exception {
-        Card.intialize();
-        HashMap<String, Integer> hm = 
-                     new HashMap<String, Integer>();
-  
-        // Adding mappings to HashMap
-        hm.put("GeeksforGeeks", 54);
-        hm.put("A computer portal", 80);
-        hm.put("For geeks", 82);
-  
-        // Printing the HashMap
-        System.out.println("Created hashmap is" + hm);
-  
-        // Loop through the hashmap
-        System.out.println("HashMap after adding bonus marks:");
-  
-        // Using for-each loop
-        for (Map.Entry mapElement : hm.entrySet()) {
-            String key = (String)mapElement.getKey();
-  
-            // Add some bonus marks
-            // to all the students and print it
-            int value = ((int)mapElement.getValue() + 10);
-  
-            System.out.println(key + " : " + value);
-        }
+        
         User.initialize();
         new LoginMenu().run();
     }
+    
+    @Test
+    public void checkTaest()
+    {
+        Assert.assertEquals("alireza", "alireza");
+    }
+
     
 
 }
---
+++ src/test/TestFile.java
@@ -0,0 +1,13 @@
+import static org.junit.Assume.assumeNoException;
+
+import org.junit.Test;
+
+public class TestFile {
+    public static void main(String[] args) {
+        createFolder2();
+    }
+    @Test
+    public static void createFolder2() throws Exception{
+        assumeNoException(new Exception());
+    }
+}
