d1d602fd46a777762a9aaa350bcddc88ea9ea7c4
alirezahaqi <ali123haghi@gmail.com>
2021-06-25 18:01:26 +0430

    draw have been fixed

--- src/main/java/controller/DuelController.java
+++ src/main/java/controller/DuelController.java
@@ -113,7 +113,7 @@ public class DuelController {
             {
                 ans.add(getZone(Zone.get("hand", duel.getCurrentPlayer())).get(i).getId());
             }
-            duel.changeZone(duelMenu.selective(ans, 1, "please select one card from your hand to reomve", "return_t").get(0), Zone.get("graveyard", duel.getCurrentPlayer()), CardState.NONE, null);
+            duel.changeZone(duelMenu.selective(ans, 1, "please select one card from your hand to reomve").get(0), Zone.get("graveyard", duel.getCurrentPlayer()), CardState.NONE, duelMenu);
 
         }
         Zone deck = Zone.get("deck", duel.getCurrentPlayer());
