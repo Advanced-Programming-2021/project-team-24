989a8a7d7194843e718c756bf2194089dd8d08e4
alirezahaqi <ali123haghi@gmail.com>
2021-07-21 14:51:21 +0430

    اثسام

--- src/main/java/server/ChatClient.java
+++ src/main/java/server/ChatClient.java
@@ -1,5 +1,27 @@
 package server;
 
+import java.io.IOException;
+
+import controller.Message;
+import controller.client.Client;
+
 public class ChatClient {
     
+
+    public static Message sendMessage(String message) throws IOException{
+        return (Client.getResponse("send --message " + message).getMessage());        
+    }
+    public static Message sendSingleRequest() throws IOException
+    {
+        return Client.getResponse("--request 1").getMessage();        
+    }
+    public static Message sendMatchRequest() throws IOException{
+        return Client.getResponse("--request 3").getMessage();
+    }
+    public static Message ignoreSingleRequest() throws IOException{
+        return Client.getResponse("--request --ignore 1").getMessage();        
+    }
+    public static Message ignoreMatchRequest() throws IOException{
+        return Client.getResponse("--request --ignore 3").getMessage();
+    }    
 }
