3791e6a48d2d5e42813151687e1d0d4fb4d792ac
tHESAMegit <hesamsoleymani20@gmail.com>
2021-07-21 10:20:06 +0430

    client

---
+++ src/main/java/controller/client/Client.java
@@ -0,0 +1,55 @@
+package controller.client;
+
+import model.Request;
+import model.Response;
+import model.Situation;
+import model.user.Player;
+import model.user.User;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    private static String token = "";
+    private static Situation currentSituation = Situation.LOGIN;
+    private static Socket socket;
+    private static DataInputStream dataInputStream;
+    private static DataOutputStream dataOutputStream;
+
+    public static void initializeNetwork() {
+        try {
+            socket = new Socket("localhost", 7777);
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException x) {
+            x.printStackTrace();
+        }
+    }
+    public static void setAfterResponse(Response response){
+        if (response.getToken() != null) token = response.getToken();
+        currentSituation = response.getCurrentSituation();
+    }
+    public static void runApp() {
+        initializeNetwork();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            try {
+                String command = scanner.nextLine();
+                Request request = new Request(command, currentSituation, token);
+                if (command.equals("menu exit") && currentSituation == Situation.LOGIN) System.exit(0);
+                dataOutputStream.writeUTF(GsonConverter.serialize(request));
+                dataOutputStream.flush();
+                String result = dataInputStream.readUTF();
+                Response response = (Response) GsonConverter.deserialize(result, Response.class);
+                setAfterResponse(response);
+                System.out.println(response.getMessage().getContent());
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/main/java/controller/client/TokenManager.java
@@ -0,0 +1,21 @@
+package controller.client;
+
+import model.user.User;
+
+import java.util.HashMap;
+
+public class TokenManager {
+    private static final HashMap<String, User> loggedInUsers = new HashMap<>();
+    public static String addUser(User user, String token) {
+        synchronized (loggedInUsers) {
+            loggedInUsers.put(token, user);
+        }
+        return token;
+    }
+
+    public static User getUser(String token) {
+        synchronized (loggedInUsers) {
+            return loggedInUsers.get(token);
+        }
+    }
+}
